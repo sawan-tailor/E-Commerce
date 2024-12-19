24a3e7c0e16b38d0181a20d3cd17fa19c0a21585
sawan-tailor <sawantailor588@gmail.com>
2024-12-19 11:37:56 +0500

    Message

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          magni ab alias qui molestias voluptatem, dicta non cupiditate! Placeat
+          nobis nesciunt asperiores delectus dolore incidunt explicabo,
+          blanditiis mollitia, voluptas dolores laboriosam at aspernatur
+          impedit! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Soluta molestias similique quas saepe repudiandae
+        </p>
+        <p>
+         
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          magni ab alias qui molestias voluptatem, dicta non cupiditate! Placeat
+          nobis nesciunt asperiores delectus dolore incidunt explicabo
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
